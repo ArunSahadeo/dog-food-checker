02b2d378f10e20878c19fa9595008333d3fcab26
Arun Sahadeo <arun@wirebox.co.uk>
2018-03-26 14:35:29 +0100

    Initial commit

---
+++ check-food.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'uri'
+
+printf("Please enter a food:\n")
+food = gets.chomp.downcase
+
+while food.empty?
+	printf("Please enter a food:\n")
+	food = gets.chomp
+end
+
+searchURI = URI::HTTP.build(:host => "canigivemydog.com", :query => URI.encode_www_form(s: food))
+
+page = Nokogiri::HTML(open(searchURI))
+firstLink = page.css("article.first a")[0]["href"]
+
+if not firstLink.length
+	printf("#{firstLink} is empty\n")
+end
+
+foodPage = Nokogiri::HTML(open(firstLink))
+
+answer = foodPage.css("#page h2").text.downcase
+
+answer = answer.split("answer:")[1].lstrip.capitalize
+
+printf("%s\n", answer)
